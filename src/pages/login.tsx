19c5144fbe290bfc53f1d4899a4da619cb93fbbf
Jadson Santos <jadson44.santos@gmail.com>
2021-08-10 23:35:20 -0300

    iniciado tela login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 import {
-    Container
+    Container,
+    SectionLogin,
+    SectionCreateAccount,
+    Title,
+    Paragraph,
+    ButtonWhite
 } from '../styles/login.style'
 
 
@@ -11,14 +16,23 @@ const Login: React.FC = () => {
 
   return (
     <Container>
+      <SectionLogin />
+      <SectionCreateAccount>
+         <Title>Seja Bem Vindo</Title>
+         <Paragraph>
+            &nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Sistema universal de recomendação de Locais
+            <br/><br/>
 
-        
+            Um website gratuito para os usuários compartilharem a sua experiência com determinado local do mundo, de forma que outras pessoas consigam saber que você viveu essa experência e quer que outras pessoas tenha essa mesma sensação em suas vidas smiling_face_with_three_hearts. O nosso objetivo é simples, em defender que bons momentos devem ser compartilhados de uma forma simples.
+
+         </Paragraph>
+         <ButtonWhite>Criar Conta</ButtonWhite>
+      </SectionCreateAccount>
     </Container>    
     );
 
 
 }
-
 
 
 export default Login;
