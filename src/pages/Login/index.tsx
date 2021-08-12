7b70e93e8999f35f9ee5bb79769e01adf9a382d6
Jadson Santos <jadson44.santos@gmail.com>
2021-08-11 22:52:25 -0300

    frontned: Atualizado tela Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,55 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
 import { 
   Container,
-  Section
+  Section,
+  SectionMain,
+  Title,
+  Header,
+  Button,
+  Logo,
+  Form
 } from './styles';
 
 const Login: React.FC = () => {
   return (
       <Container>
-        <Section backgroundColor="var(--white)" width="60%"></Section>
+        <SectionMain backgroundColor="var(--white)" width="60%">
+          <Header>
+            <Logo src="/assets/logo.png" />
+          </Header>
+          <Title fontSize="46px" color="var(--red)" >Entre em sua conta</Title>
+          <Form>
+            <TextField style={{width: "460px", height: "60px" }} color="secondary"  id="input-username" label="username" variant="outlined" />
+            <TextField style={{width: "460px", height: "60px" }} color="secondary" label="Senha" variant="outlined" />
+            <div style={{width: "460px",display: "flex", alignItems: "center",justifyContent: "space-between"}} >
+              <FormControlLabel control={<Checkbox  />} label="Me lembre" />
+              <a href="/esqueci_minha_senha">privacidade</a>
+            </div>
+            <Button backgroundColor="var(--red)" color="var(--white)" fontSize="18px" height="60px" width="223px"  >Entrar</Button>
+          </Form>
+          <div style={{width: "460px", height: "30px",display: "flex", alignItems: "center",justifyContent: "center"}} >
+            <a href="/privacidade">privacidade</a>
+            <div style={{width: "5px", height: "5px",margin: "0px 20px 0px 20px", borderRadius: "17px", backgroundColor: "#C4C4C4"}} > </div>
+            <a href="/termos_de_uso">termos de uso</a>
+          </div>
+        </SectionMain>
         <Section backgroundColor="var(--red)" width="40.43927648578811%">
-          <h1>Login</h1>
-          <p></p>
-          <button></button>
+          <Title fontSize="36px" color="var(--white)" >Seja BemVindo</Title>
+          <p style={{ width: "90%",textAlign: "center" , color: "var(--white)"  }} >Sistema universal de recomendação de Locais</p>
+          <p style={{ width: "90%",textAlign: "justify", color: "var(--white)"  }} >
+            Um website gratuito para os usuários compartilharem a 
+            sua experiência com determinado local do mundo, de fo
+            rma que outras pessoas consigam saber que você viveu 
+            essa experência e quer que outras pessoas tenha essa 
+            mesma sensação em suas vidas.
+            O nosso objetivo é simples, em defender que bons momentos de
+            vem ser compartilhados de uma forma simples.
+          </p>
+          <Button backgroundColor="var(--white)" color="var(--red)" fontSize="18px" height="60px" width="223px"  >Criar Conta</Button>
         </Section>
       </Container>
   );
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -9,6 +9,7 @@ export const Container = styled.div`
     height: 100%;
     background-color: var(--white);
     display: flex;
+    font-family: 'Roboto',sans-serif;
 `;
 
 interface SectionProps {
@@ -20,5 +21,80 @@ export const Section = styled.div<SectionProps>`
     height: 100%;
     width: ${props => props.width };
     background-color: ${props => props.backgroundColor};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+
+`
+
+export const SectionMain = styled.div<SectionProps>`
+    height: 100%;
+    width: ${props => props.width };
+    background-color: ${props => props.backgroundColor};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+`
+
+
+interface TitleProps {
+    fontSize: string;
+    color: string
+}
+
+
+export const Title = styled.h2<TitleProps>`
+    font-size: ${props => props.fontSize};
+    color: ${props => props.color};
+
+    &::after {
+        content: '';
+        border-bottom: 3px solid  ${props => props.color};
+        width: 80px;
+        height: 8px;
+        display: block;
+        margin: 0 auto;
+    } 
+`
+
+interface ButtonProps {
+    width: string
+    height: string
+    color: string
+    backgroundColor: string
+    fontSize: string
+}
+
+export const Button = styled.button<ButtonProps>`
+    width: ${props => props.width };
+    height: ${props => props.height };
+    background-color: ${props => props.backgroundColor};
+    font-size: ${props => props.fontSize};
+    border-radius: 0.3em;
+    color: ${props => props.color};
+    cursor: pointer;
+`
+
+export const Header = styled.header`
+    width: 100%;
+    height: 70px;
+    padding-left: 30px;
+    margin-bottom: 35px;
+    display: flex;
+    align-items: center;
+`
+export const Logo = styled.img`
+    width: 151px;
+    height: 29px;
+`
+export const Form = styled.form`
+    width: 100%;
+    height: 350px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
 
 `
