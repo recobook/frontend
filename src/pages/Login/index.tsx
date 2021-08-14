b14a371ce314ad25de99db6299c8c66a9349ea2e
Jadson Santos <jadson44.santos@gmail.com>
2021-08-14 18:07:33 -0300

    frontend: Atualizado label username da tela de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -41,7 +41,7 @@ const Login: React.FC = () => {
           </Header>
           <Title fontSize="46px" color="var(--red)" >Entre em sua conta</Title>
           <Form onSubmit={onSubmit}>
-            <TextField className="inputs" color="secondary" name="username" label="username" variant="outlined" required />
+            <TextField className="inputs" color="secondary" name="username" label="Username" variant="outlined" required />
             <TextField className="inputs" color="secondary" name="password" label="Senha" type="password" variant="outlined" required/>
             {
               firstTry?<></>:<Alert severity={data?.status === 400 ? "error":"success"}>{data?.data?.message}</Alert>
