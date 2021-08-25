0d562524ec23e0e4b5aea6c12a80f078a32bf8f6
Jadson Santos <jadson44.santos@gmail.com>
2021-08-25 20:37:42 -0300

    frontend: Atualizando api core para localhost

--- src/utils/api_core.ts
+++ src/utils/api_core.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: "http://192.168.0.21:8000", 
+    baseURL: "http://0.0.0.0:8000", 
     headers: {'Content-Type': 'application/json'},
 })
